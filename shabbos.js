d73a51541debbea8b9ec986d6880dc00a3ef01cb
Matthew Polaniecki <matpol88@gmail.com>
2020-01-19 12:03:33 -0500

    updated styles and fetch

--- shabbos.js
+++ shabbos.js
@@ -1,25 +1,28 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var shabbos = JSON.parse(this.responseText);
-    
-    document.getElementById("parsha").innerHTML = "This week's Torah portion is: "+shabbos.items[1].title;
-    
-    var d = new Date(shabbos.items[0].date);
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var formattedDate = days[d.getDay()]+ " " + d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
-    document.getElementById("candleLighting").innerHTML = formattedDate +  "<br>" + shabbos.items[0].title  ;
-
-    var e = new Date(shabbos.items[2].date);
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var formattedDate = days[e.getDay()]+ " " + e.getDate() + "-" + (e.getMonth() + 1) + "-" + e.getFullYear();
-    document.getElementById("havdala").innerHTML = formattedDate + "<br>" + shabbos.items[2].title;
-  }
-};
-xmlhttp.open("GET", "https://www.hebcal.com/shabbat/?cfg=json&geonameid=5111974&m=50", true);
-xmlhttp.send();
-
 function changeColor(){
-var body = document.querySelector("body");
-body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    var body = document.querySelector("body");
+    body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
 }
+
+fetch('https://www.hebcal.com/shabbat/?cfg=json&geonameid=5111974&m=50')
+.then(
+  function(response) {
+    response.json().then(function(data) {
+      //get parsha
+      document.getElementById('parsha').innerHTML = "This week's Torah portion is: "+ data.items[1].title;
+
+      //get candlelighting
+      var d = new Date(data.items[0].date);
+      var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      var formattedDate = days[d.getDay()]+ " " + (d.getMonth() + 1) +"-" + d.getDate()+ "-" + d.getFullYear();
+      document.getElementById("candleLighting").innerHTML = formattedDate +  "<br>" + data.items[0].title  ;
+
+      //get havdala
+      var e = new Date(data.items[2].date);
+      var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      var formattedDate = days[e.getDay()]+ " " + (e.getMonth() + 1) + "-" + e.getDate() + "-" + e.getFullYear();
+      document.getElementById("havdala").innerHTML = formattedDate + "<br>" + data.items[2].title;
+
+  }
+  );
+}
+)

5c76829d130b5a7abb8ac7828ca030bef8ad9ab6
matpol88 <matpol88@gmail.com>
2020-02-09 11:11:58 -0500

    cleaned up code

--- dailydaf/daf.js
+++ dailydaf/daf.js
@@ -1,85 +1,83 @@
-
 var date;
 var next;
 var prev;
-var dafLength;
 var enPasuk;
-fetch('https://www.sefaria.org/api/calendars/')
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(myJson) {
-   document.getElementById('date').innerHTML = myJson.date;
-   document.getElementById('pages').innerText = myJson.calendar_items[3].url;
-   date = myJson.calendar_items[3].url;
-   fetch('https://www.sefaria.org/api/texts/' + date)
-  .then(function(response) {
-    return response.json()
-  })
- .then(function(textPull){
-   enPasuk = textPull.text;
-   hePasuk = textPull.he;
-   count = 0;
-   for (var pasuk of enPasuk) {
-     var i = pasuk;
-    var ul = document.getElementById("enText"); 
-    var li = document.createElement("li");
-    li.innerHTML = hePasuk[count] + "<br>"+"<br>" + "<span>" + pasuk + "</span> <br> <br>";
-    ul.appendChild(li);
-    count++;
-  }
-  document.getElementById("next").innerHTML = textPull.next;
-  document.getElementById("prev").innerHTML = textPull.prev;
-  next = textPull.next;
-  prev = textPull.prev;
- })
-  })
 
-function nextPage(){
-  fetch('https://www.sefaria.org/api/texts/' + next)
-  .then(function(response){
+//get today's date and page number
+fetch('https://www.sefaria.org/api/calendars/')
+  .then(function (response) {
     return response.json()
   })
-  .then(function(nextPull){
-    enPasuk = nextPull.text;
-   hePasuk = nextPull.he;
-   count = 0;
-   for (var pasuk of enPasuk) {
-     var i = pasuk;
-    var ul = document.getElementById("enText"); 
-    var li = document.createElement("li");
-    li.innerHTML = hePasuk[count] + "<br>"+"<br>" + "<span>" + pasuk + "</span> <br> <br>";
-    ul.appendChild(li);
-    count++;
-  }
-  document.getElementById("next").innerHTML = nextPull.next;
-  document.getElementById("prev").innerHTML = nextPull.prev;
-  next = nextPull.next;
-  prev = nextPull.prev;
+  .then(function (myJson) {
+    document.getElementById('date').innerHTML = myJson.date;
+    document.getElementById('pages').innerText = myJson.calendar_items[3].url;
+    date = myJson.calendar_items[3].url;
+    fetch('https://www.sefaria.org/api/texts/' + date)
+      .then(function (response) {
+        return response.json()
+      }) //get today's text
+      .then(function (textPull) {
+        enPasuk = textPull.text;
+        hePasuk = textPull.he;
+        count = 0;
+        for (var pasuk of enPasuk) {
+          var ul = document.getElementById("enText");
+          var li = document.createElement("li");
+          li.innerHTML = hePasuk[count] + "<br> <br> <span>" + pasuk + "</span> <br> <br>";
+          ul.appendChild(li);
+          count++;
+        }
+        document.getElementById("next").innerHTML = textPull.next;
+        document.getElementById("prev").innerHTML = textPull.prev;
+        next = textPull.next;
+        prev = textPull.prev;
+      })
   })
+//get next page
+function nextPage() {
+  fetch('https://www.sefaria.org/api/texts/' + next)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (nextPull) {
+      enPasuk = nextPull.text;
+      hePasuk = nextPull.he;
+      count = 0;
+      for (var pasuk of enPasuk) {
+        var ul = document.getElementById("enText");
+        var li = document.createElement("li");
+        li.innerHTML = hePasuk[count] + "<br> <br> <span>" + pasuk + "</span> <br> <br>";
+        ul.appendChild(li);
+        count++;
+      }
+      document.getElementById("next").innerHTML = nextPull.next;
+      document.getElementById("prev").innerHTML = nextPull.prev;
+      next = nextPull.next;
+      prev = nextPull.prev;
+    })
 }
 
-
-function prevPage(){
+//get previous page
+function prevPage() {
   fetch('https://www.sefaria.org/api/texts/' + prev)
-  .then(function(response){
-    return response.json()
-  })
-  .then(function(prevPull){
-    enPasuk = prevPull.text;
-    hePasuk = prevPull.he;
-    count = 0;
-    for (var pasuk of enPasuk) {
-      var i = pasuk;
-     var ul = document.getElementById("prevText"); 
-     var li = document.createElement("li");
-     li.innerHTML = hePasuk[count] + "<br>"+"<br>" + "<span>" + pasuk + "</span> <br> <br>";
-     ul.appendChild(li);
-     count++;
-   }
-   document.getElementById("next").innerHTML = prevPull.next;
-   document.getElementById("prev").innerHTML = prevPull.prev;
-    next = prevPull.next;
-    prev = prevPull.prev;
-  })
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (prevPull) {
+      enPasuk = prevPull.text;
+      hePasuk = prevPull.he;
+      count = 0;
+      for (var pasuk of enPasuk) {
+        var ul = document.getElementById("prevText");
+        var li = document.createElement("li");
+        document.getElementById('prevText').style.display = 'block'
+        li.innerHTML = hePasuk[count] + "<br> <br> <span>" + pasuk + "</span> <br> <br>";
+        ul.appendChild(li);
+        count++;
+      }
+      document.getElementById("next").innerHTML = prevPull.next;
+      document.getElementById("prev").innerHTML = prevPull.prev;
+      next = prevPull.next;
+      prev = prevPull.prev;
+    })
 }
--- shabbostime/shabbos.js
+++ shabbostime/shabbos.js
@@ -1,28 +1,28 @@
-function changeColor(){
-    var body = document.querySelector("body");
-    body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+function changeColor() {
+  var body = document.querySelector("body");
+  body.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 fetch('https://www.hebcal.com/shabbat/?cfg=json&geonameid=5111974&m=50')
-.then(
-  function(response) {
-    response.json().then(function(data) {
-      //get parsha
-      document.getElementById('parsha').innerHTML = data.items[1].title;
+  .then(
+    function (response) {
+      response.json().then(function (data) {
+        //get parsha
+        document.getElementById('parsha').innerHTML = data.items[1].title;
 
-      //get candlelighting
-      var d = new Date(data.items[0].date);
-      var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      var formattedDate = days[d.getDay()]+ " " + (d.getMonth() + 1) +"-" + d.getDate()+ "-" + d.getFullYear();
-      document.getElementById("candleLighting").innerHTML = formattedDate +  "<br>" + data.items[0].title  ;
+        //get candlelighting
+        var d = new Date(data.items[1].date);
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var formattedDate = days[d.getDay()] + " " + (d.getMonth() + 1) + "-" + d.getDate() + "-" + d.getFullYear();
+        document.getElementById("candleLighting").innerHTML = formattedDate + "<br>" + data.items[0].title;
 
-      //get havdala
-      var e = new Date(data.items[2].date);
-      var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      var formattedDate = days[e.getDay()]+ " " + (e.getMonth() + 1) + "-" + e.getDate() + "-" + e.getFullYear();
-      document.getElementById("havdala").innerHTML = formattedDate + "<br>" + data.items[2].title;
+        //get havdala
+        var e = new Date(data.items[2].date);
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var formattedDate = days[e.getDay()] + " " + (e.getMonth() + 1) + "-" + e.getDate() + "-" + e.getFullYear();
+        document.getElementById("havdala").innerHTML = formattedDate + "<br>" + data.items[2].title;
 
-  }
-  );
-}
-)
+      }
+      );
+    }
+  )

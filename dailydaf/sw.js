e8ffa17614b5f677a40aea123f3016d250becd75
Matt Polaniecki <matpol88@gmail.com>
2020-07-16 11:28:43 -0400

    cachename check

--- dailydaf/sw.js
+++ dailydaf/sw.js
@@ -1,19 +1,32 @@
-
-
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open(cacheName).then(function(cache) {
-        return cache.addAll(
-          [
-            '/daf.js',
-            '/daf.css',
-            '/index.html'
-          ]
-        );
-      })
-    );
+    var CACHE_NAME = 'my-site-cache-v1';
+    var urlsToCache = [
+      '/',
+      'index.html',
+      'daf.css',
+      'daf.js'
+    ];
+    
+    self.addEventListener('install', function(event) {
+      // Perform install steps
+      event.waitUntil(
+        caches.open(CACHE_NAME)
+          .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+          })
+      );
+    });
   });
 
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request));
-  });
+  let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+});
+

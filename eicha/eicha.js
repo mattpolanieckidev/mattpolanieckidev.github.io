199f36a9334e98223d4e07fe7cff926a32ceee4f
matpol88 <matpol88@gmail.com>
2020-07-30 12:50:37 -0400

    write eicha divs properly

--- eicha/eicha.js
+++ eicha/eicha.js
@@ -2,6 +2,52 @@ var date;
 var next;
 var prev;
 var enPasuk;
+var hePasuk;
+var amudCount;
+var section;
+var currentAmud;
+var masechtaProgress;
+var masechtaSearch = document.getElementById('masechta');
+var pageTitle = document.getElementById("pageTitle");
+var textdiv = document.getElementById("content");
+var div;
+var heading;
+var ul;
+var pasuk;
+var newHeading;
+var versionNum;
+var versionDate;
+var links;
+
+//create and append Div, Heading, and Unordered List
+function createDiv(){
+  div = document.createElement("div");
+  div.classList.add("textContent");
+  textdiv.appendChild(div);
+  heading = document.createElement("h3");
+  heading.className = "pageHeading";
+  div.appendChild(heading);
+  ul = document.createElement("ul");
+  ul.classList.add("pageText");
+  div.appendChild(ul);
+}
+
+//Loop through available pasukim and append to unordered list. Check to see if the translation should be shown or not. 
+function writePasuk(){
+  for (var pasuk of enPasuk) {
+    var li = document.createElement("li");
+    if (localStorage.getItem("hidden") === "hidden"){
+    li.innerHTML = hePasuk[count] + "<p class=\"english hidden\">" + pasuk + "</p>";
+    ul.appendChild(li);
+    count++;
+    }
+    else {
+      li.innerHTML = hePasuk[count] + "<p class=\"english\">" + pasuk + "</p>";
+      ul.appendChild(li);
+      count++;
+    }
+  }
+}
 
 //get today's date and page number
 fetch('https://www.sefaria.org/api/texts/eicha')
@@ -12,40 +58,34 @@ fetch('https://www.sefaria.org/api/texts/eicha')
       enPasuk = textPull.text;
       hePasuk = textPull.he;
       count = 0;
-      for (var pasuk of enPasuk) {
-        var ul = document.getElementById("enText");
-        var li = document.createElement("li");
-        li.innerHTML = hePasuk[count] + "<br>" + "<br> <p class=\"english\">" + pasuk + "<br> <br> </p>";
-        ul.appendChild(li);
-        count++;
-      }
+      createDiv();
+      heading.innerHTML = textPull.ref;
+      writePasuk();
       document.getElementById("next").innerHTML = textPull.next;
       next = textPull.next;
       prev = textPull.prev;
     })
+
 //get next page
 function nextPage() {
   fetch('https://www.sefaria.org/api/texts/' + next)
     .then(function (response) {
       return response.json()
     })
-    .then(function (nextPull) {
-      enPasuk = nextPull.text;
-      hePasuk = nextPull.he;
+    .then(function (pull) {
+      enPasuk = pull.text;
+      hePasuk = pull.he;
       count = 0;
-      for (var pasuk of enPasuk) {
-        var ul = document.getElementById("enText");
-        var li = document.createElement("li");
-        li.innerHTML = hePasuk[count] +"<br>" + "<br> <p class=\"english\">" + pasuk + "<br> <br> </p>";
-        ul.appendChild(li);
-        count++;
-      }
-      document.getElementById("next").innerHTML = nextPull.next;
-      next = nextPull.next;
+      createDiv();
+      heading.innerHTML = pull.ref;
+      writePasuk();
+      heading.scrollIntoView({behavior:"auto", block:"center"});
+      document.getElementById("next").innerHTML = pull.next;
+      next = pull.next;
     })
 }
 
-var current = document.getElementById("enText").childNodes;
+
 //get previous page
 /*function prevPage() {
   fetch('https://www.sefaria.org/api/texts/' + prev)

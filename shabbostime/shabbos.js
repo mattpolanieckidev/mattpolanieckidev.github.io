b263e45c6197c305735cdb8b2ee8e6bad722316c
matpol88 <matpol88@gmail.com>
2020-08-25 23:06:26 -0400

    fixed color formula

--- shabbostime/shabbos.js
+++ shabbostime/shabbos.js
@@ -13,9 +13,12 @@ if ('serviceWorker' in navigator) {
 
 function changeColor() {
   var body = document.querySelector("body");
-  var color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  var x=Math.round(0xffffff * Math.random()).toString(16);
+  var y=(6-x.length);
+  var z='000000';
+  var z1 = z.substring(0,y);
+  var color= '#' + z1 + x;
   body.style.backgroundColor = color;
-  
   document.getElementById('colorlabel2').innerHTML = "Shabbos Times " + color;
 }
 

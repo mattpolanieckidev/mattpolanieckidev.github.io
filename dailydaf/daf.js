89db127414fa2e0901d08080a3926cb608bdaa3d
Matt Polaniecki <matpol88@gmail.com>
2023-03-24 12:24:40 -0400

    ChatGPT wrote this

--- dailydaf/daf.js
+++ dailydaf/daf.js
@@ -46,65 +46,54 @@ function createDiv(){
 }
 
 //Loop through available pasukim and append to unordered list. Check to see if the translation should be shown or not.
-function writePasuk(){
-  for (var pasuk of enPasuk) {
-    var li = document.createElement("li");
-    if (localStorage.getItem("hidden") === "hidden"){
-    li.innerHTML = hePasuk[count] + "<p class=\"english hidden\">" + pasuk + "</p>";
+function writePasuk() {
+  enPasuk.forEach((pasuk, index) => {
+    const li = document.createElement("li");
+    const isHidden = localStorage.getItem("hidden") === "hidden";
+    const englishClass = isHidden ? "english hidden" : "english";
+    li.innerHTML = `${hePasuk[index]}<p class="${englishClass}">${pasuk}</p>`;
     ul.appendChild(li);
-    count++;
-    document.getElementById("switch2").checked = false;
-    adjustFont(localStorage.getItem("size"));
-    }
-    else {
-      li.innerHTML = hePasuk[count] + "<p class=\"english\">" + pasuk + "</p>";
-      ul.appendChild(li);
-      count++;
-      document.getElementById("switch2").checked = true;
-      adjustFont(localStorage.getItem("size"));
-    }
-  }
+  });
+  
+  const isHidden = localStorage.getItem("hidden") === "hidden";
+  document.getElementById("switch2").checked = !isHidden;
+  adjustFont(localStorage.getItem("size"));
 }
 
+
 //dark mode
-function night(){
-  if (localStorage.getItem("night") === "light"){
-  document.getElementById("body").classList.toggle("night");
-  document.getElementById("toggleclose").classList.toggle("nightcolor");
-  document.getElementById("masechta").classList.toggle("nightcolor");
-  document.getElementById("modal").classList.toggle("bg-dark");
-  document.getElementById("nav").classList.toggle("bg-dark");
-  document.getElementById("switch1").checked = true;
-  localStorage.setItem("night", "dark");}
-  else{
-    document.getElementById("body").classList.toggle("night");
-    document.getElementById("toggleclose").classList.toggle("nightcolor");
-    document.getElementById("masechta").classList.toggle("nightcolor");
-    document.getElementById("modal").classList.toggle("bg-dark");
-    document.getElementById("nav").classList.toggle("bg-dark");
-    document.getElementById("switch1").checked = false;
-    localStorage.setItem("night", "light");
-  }
+function night() {
+  // Get the current value of the "night" key from `localStorage`
+  const isNight = localStorage.getItem("night") === "dark";
+  
+  // Toggle the "night" class on the body, toggle the "nightcolor" class on elements, and toggle the "bg-dark" class on elements
+  document.getElementById("body").classList.toggle("night", !isNight);
+  document.getElementById("toggleclose").classList.toggle("nightcolor", !isNight);
+  document.getElementById("masechta").classList.toggle("nightcolor", !isNight);
+  document.getElementById("modal").classList.toggle("bg-dark", !isNight);
+  document.getElementById("nav").classList.toggle("bg-dark", !isNight);
+  
+  // Update the state of the "switch1" checkbox and the value of the "night" key in `localStorage`
+  document.getElementById("switch1").checked = isNight;
+  localStorage.setItem("night", isNight ? "light" : "dark");
 }
+
+
 
 //get today's date and page number
 fetch("https://www.sefaria.org/api/calendars/")
-  .then(function (response) {
-    return response.json()
-  })
-  .then(function (myJson) {
+  .then(response => response.json())
+  .then(myJson => {
     document.getElementById("date").innerHTML = myJson.date;
-    date = myJson.calendar_items.filter(i => i.title.en == "Daf Yomi")[0].url;
-    document.getElementById("pages").innerText = date;
-    fetch("https://www.sefaria.org/api/texts/" + date)
-      .then(function (response) {
-        return response.json()
-      }) //get today's text
-      .then(function (pull) {
-        amudCount = (pull.length / 2) + 1;
-        section = pull.sections.toString();
-        currentAmud = Number(section.slice(0, -1));
-        masechtaProgress = pull.book + " " + currentAmud + " of " + amudCount;
+    const dafYomi = myJson.calendar_items.find(i => i.title.en === "Daf Yomi").url;
+    document.getElementById("pages").innerText = dafYomi;
+    fetch(`https://www.sefaria.org/api/texts/${dafYomi}`)
+      .then(response => response.json())
+      .then(pull => {
+        const amudCount = (pull.length / 2) + 1;
+        const section = pull.sections.toString();
+        const currentAmud = Number(section.slice(0, -1));
+        const masechtaProgress = `${pull.book} ${currentAmud} of ${amudCount}`;
         masechtaSearch.value = pull.book;
         enPasuk = pull.text;
         hePasuk = pull.he;
@@ -117,49 +106,29 @@ fetch("https://www.sefaria.org/api/calendars/")
         next = pull.next;
         prev = pull.prev;
         pageTitle.innerText = masechtaProgress;
-        document.getElementById("next").innerHTML = pull.next + "<svg class=\"svg-icon\" viewBox=\"0 0 20 20\">" +"<path d=\"M483.058,215.613l-215.5-177.6c-4-3.3-9.6-4-14.3-1.8c-4.7,2.2-7.7,7-7.7,12.2v93.6c-104.6,3.8-176.5,40.7-213.9,109.8c-32.2,59.6-31.9,130.2-31.6,176.9c0,3.8,0,7.4,0,10.8c0,6.1,4.1,11.5,10.1,13.1c1.1,0.3,2.3,0.4,3.4,0.4c4.8,0,9.3-2.5,11.7-6.8c73-128.7,133.1-134.9,220.2-135.2v93.3c0,5.2,3,10,7.8,12.2s10.3,1.5,14.4-1.8l215.4-178.2c3.1-2.6,4.9-6.4,4.9-10.4S486.158,218.213,483.058,215.613z M272.558,375.613v-78.1c0-3.6-1.4-7-4-9.5c-2.5-2.5-6-4-9.5-4c-54.4,0-96.1,1.5-136.6,20.4c-35,16.3-65.3,44-95.2,87.5c1.2-39.7,6.4-87.1,28.1-127.2c34.4-63.6,101-95.1,203.7-96c7.4-0.1,13.4-6.1,13.4-13.5v-78.2l180.7,149.1L272.558,375.613z\"></path>" + "</svg>";
-
-        document.getElementById("prev").innerHTML = "<svg class=\"svg-icon\" viewBox=\"0 0 20 20\">" +
-        "<path d=\"M3.24,7.51c-0.146,0.142-0.146,0.381,0,0.523l5.199,5.193c0.234,0.238,0.633,0.064,0.633-0.262v-2.634c0.105-0.007,0.212-0.011,0.321-0.011c2.373,0,4.302,1.91,4.302,4.258c0,0.957-0.33,1.809-1.008,2.602c-0.259,0.307,0.084,0.762,0.451,0.572c2.336-1.195,3.73-3.408,3.73-5.924c0-3.741-3.103-6.783-6.916-6.783c-0.307,0-0.615,0.028-0.881,0.063V2.575c0-0.327-0.398-0.5-0.633-0.261L3.24,7.51 M4.027,7.771l4.301-4.3v2.073c0,0.232,0.21,0.409,0.441,0.366c0.298-0.056,0.746-0.123,1.184-0.123c3.402,0,6.172,2.709,6.172,6.041c0,1.695-0.718,3.24-1.979,4.352c0.193-0.51,0.293-1.045,0.293-1.602c0-2.76-2.266-5-5.046-5c-0.256,0-0.528,0.018-0.747,0.05C8.465,9.653,8.328,9.81,8.328,9.995v2.074L4.027,7.771z\"></path>" + "</svg>" + pull.prev;
+        document.getElementById("next").innerHTML = pull.next;
+        document.getElementById("prev").innerHTML = "<svg class=\"svg-icon\" viewBox=\"0 0 20 20\">" +"<path d=\"M3.24,7.51c-0.146,0.142-0.146,0.381,0,0.523l5.199,5.193c0.234,0.238,0.633,0.064,0.633-0.262v-2.634c0.105-0.007,0.212-0.011,0.321-0.011c2.373,0,4.302,1.91,4.302,4.258c0,0.957-0.33,1.809-1.008,2.602c-0.259,0.307,0.084,0.762,0.451,0.572c2.336-1.195,3.73-3.408,3.73-5.924c0-3.741-3.103-6.783-6.916-6.783c-0.307,0-0.615,0.028-0.881,0.063V2.575c0-0.327-0.398-0.5-0.633-0.261L3.24,7.51 M4.027,7.771l4.301-4.3v2.073c0,0.232,0.21,0.409,0.441,0.366c0.298-0.056,0.746-0.123,1.184-0.123c3.402,0,6.172,2.709,6.172,6.041c0,1.695-0.718,3.24-1.979,4.352c0.193-0.51,0.293-1.045,0.293-1.602c0-2.76-2.266-5-5.046-5c-0.256,0-0.528,0.018-0.747,0.05C8.465,9.653,8.328,9.81,8.328,9.995v2.074L4.027,7.771z\"></path> </svg>" + pull.prev;
       })
-    })
+  })
 
 //get next page
-function nextPage() {
-  fetch("https://www.sefaria.org/api/texts/" + next)
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (pull) {
-      enPasuk = pull.text;
-      hePasuk = pull.he;
-      count = 0;
-      createDiv();
-      heading.innerHTML = pull.ref;
-      writePasuk();
-      heading.scrollIntoView({behavior:"auto", block:"center"});
-      document.getElementById("next").innerHTML = pull.next;
-      next = pull.next;
-    })
+const nextPage = async () => {
+  const response = await fetch(`https://www.sefaria.org/api/texts/${next}`);
+  const { text: enPasuk, he: hePasuk, ref, next: nextPasuk } = await response.json();
+  count = 0;
+  createDiv();
+  heading.innerHTML = ref;
+  writePasuk();
+  heading.scrollIntoView({behavior:"auto", block:"center"});
+  document.getElementById("next").innerHTML = nextPasuk;
+  next = nextPasuk;
 }
 
-/*
-// Scroll and load
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-    nextPage();
-  }
-}
-*/
 //get previous page
-function prevPage() {
+const prevPage = () => {
   fetch("https://www.sefaria.org/api/texts/" + prev)
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (pull) {
+    .then(response => response.json())
+    .then(pull => {
       document.getElementById("content").textContent = "";
       enPasuk = pull.text;
       hePasuk = pull.he;
@@ -169,34 +138,24 @@ function prevPage() {
       writePasuk();
       prev = pull.prev;
       next = pull.next;
-      document.getElementById("next").innerHTML = "<svg class=\"svg-icon\" viewBox=\"0 0 20 20\">" + 
-      "<path d=\"M483.058,215.613l-215.5-177.6c-4-3.3-9.6-4-14.3-1.8c-4.7,2.2-7.7,7-7.7,12.2v93.6c-104.6,3.8-176.5,40.7-213.9,109.8c-32.2,59.6-31.9,130.2-31.6,176.9c0,3.8,0,7.4,0,10.8c0,6.1,4.1,11.5,10.1,13.1c1.1,0.3,2.3,0.4,3.4,0.4c4.8,0,9.3-2.5,11.7-6.8c73-128.7,133.1-134.9,220.2-135.2v93.3c0,5.2,3,10,7.8,12.2s10.3,1.5,14.4-1.8l215.4-178.2c3.1-2.6,4.9-6.4,4.9-10.4S486.158,218.213,483.058,215.613z M272.558,375.613v-78.1c0-3.6-1.4-7-4-9.5c-2.5-2.5-6-4-9.5-4c-54.4,0-96.1,1.5-136.6,20.4c-35,16.3-65.3,44-95.2,87.5c1.2-39.7,6.4-87.1,28.1-127.2c34.4-63.6,101-95.1,203.7-96c7.4-0.1,13.4-6.1,13.4-13.5v-78.2l180.7,149.1L272.558,375.613z\"></path>" + "</svg>" + pull.next;
-      
-      document.getElementById("prev").innerHTML = "<svg class=\"svg-icon\" viewBox=\"0 0 20 20\">" +
-        "<path d=\"M3.24,7.51c-0.146,0.142-0.146,0.381,0,0.523l5.199,5.193c0.234,0.238,0.633,0.064,0.633-0.262v-2.634c0.105-0.007,0.212-0.011,0.321-0.011c2.373,0,4.302,1.91,4.302,4.258c0,0.957-0.33,1.809-1.008,2.602c-0.259,0.307,0.084,0.762,0.451,0.572c2.336-1.195,3.73-3.408,3.73-5.924c0-3.741-3.103-6.783-6.916-6.783c-0.307,0-0.615,0.028-0.881,0.063V2.575c0-0.327-0.398-0.5-0.633-0.261L3.24,7.51 M4.027,7.771l4.301-4.3v2.073c0,0.232,0.21,0.409,0.441,0.366c0.298-0.056,0.746-0.123,1.184-0.123c3.402,0,6.172,2.709,6.172,6.041c0,1.695-0.718,3.24-1.979,4.352c0.193-0.51,0.293-1.045,0.293-1.602c0-2.76-2.266-5-5.046-5c-0.256,0-0.528,0.018-0.747,0.05C8.465,9.653,8.328,9.81,8.328,9.995v2.074L4.027,7.771z\"></path>" + "</svg>" + pull.prev;
-    })
+      document.getElementById("next").innerHTML = next;
+      document.getElementById("prev").innerHTML = prev;
+    });
 }
 
-function hide(){
-  var x = document.getElementsByClassName("english");
-  for (i = 0; i < x.length; i++) {
+const hide = () => {
+  const x = document.getElementsByClassName("english");
+  for (let i = 0; i < x.length; i++) {
     x.item(i).classList.toggle("hidden");
   }
-  if (localStorage.getItem("hidden") === "show"){
-  localStorage.setItem("hidden", "hidden");}
-    else{
-      localStorage.setItem("hidden", "show");
-    }
-
+  localStorage.setItem("hidden", localStorage.getItem("hidden") === "show" ? "hidden" : "show");
 }
 
+
 //set slider value on mobile
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    adjustFont("1");
-  } else {
-    adjustFont("2");
-  }
+const myFunction = x => {
+  const fontValue = x.matches ? "1" : "2";
+  adjustFont(fontValue);
 }
 
 var x = window.matchMedia("(orientation:portrait)")
@@ -224,35 +183,34 @@ function adjustFont(a){
   };
 
 //progress bar for full daf cycle
-let date1 = new Date("1/5/2020")
-let date2 = new Date("6/7/2027");
-let today = new Date();
-var difference_in_time = today.getTime() -  date1.getTime();
-var Difference_In_Days = difference_in_time / (1000 * 3600 * 24);
-var progressNum = ((Difference_In_Days *100) / 2710).toFixed(2);
-var progress = document.getElementById("progress");
-progress.style.width = (progressNum) +"%";
-progress.innerHTML = "Siyum HaShas "+ progressNum +"%";
+const startDate = new Date("1/5/2020");
+const endDate = new Date("6/7/2027");
+const today = new Date();
+const daysElapsed = Math.floor((today - startDate) / (1000 * 3600 * 24));
+const totalDays = Math.floor((endDate - startDate) / (1000 * 3600 * 24));
+const progressNum = ((daysElapsed / totalDays) * 100).toFixed(2);
+const progress = document.getElementById("progress");
+progress.style.width = `${progressNum}%`;
+progress.innerHTML = `Siyum HaShas ${progressNum}%`;
 
 //find a daf
-function getPage() {
-  var masechtaName = document.getElementById("masechta").value;
-  var dafNum = document.getElementById("pageSelect").value;
-   fetch("https://www.sefaria.org/api/texts/" + masechtaName + "." + dafNum)
-   .then(function (response) {
-       return response.json()
-   }) //get text
-   .then(function (pull) {
-    document.getElementById("content").textContent = "";
-    enPasuk = pull.text;
-    hePasuk = pull.he;
-    count = 0;
-    createDiv();
-    heading.innerHTML = pull.ref;
-    writePasuk();
-    document.getElementById("prev").innerHTML = pull.prev;
-    document.getElementById("next").innerHTML = pull.next;
-    next = pull.next;
-    prev = pull.prev;
-   })
- }
+const getPage = () => {
+  const masechtaName = document.getElementById("masechta").value;
+  const dafNum = document.getElementById("pageSelect").value;
+  
+  fetch(`https://www.sefaria.org/api/texts/${masechtaName}.${dafNum}`)
+    .then(response => response.json())
+    .then(pull => {
+      document.getElementById("content").textContent = "";
+      enPasuk = pull.text;
+      hePasuk = pull.he;
+      count = 0;
+      createDiv();
+      heading.innerHTML = pull.ref;
+      writePasuk();
+      document.getElementById("prev").innerHTML = pull.prev;
+      document.getElementById("next").innerHTML = pull.next;
+      next = pull.next;
+      prev = pull.prev;
+    })
+}

831fa95fb000382ea33c751e1276ef099dd12071
Matt Polaniecki <matpol88@gmail.com>
2023-04-05 13:17:22 -0400

    updated for 2023

--- sefira/sefira.js
+++ sefira/sefira.js
@@ -1,109 +1,108 @@
-var omer=[
-{hebrew:'הַיּוֹם שְׁנֵי יָמִים לָעֹמֶר',english:'Today is One Day of the Omer'},
-{hebrew:'הַיּוֹם שְׁנֵי יָמִים לָעֹמֶר',english:'Today is Two Days of the Omer'},
-{hebrew:'הַיּוֹם שְׁלֹשָׁה יָמִים לָעֹמֶר',english:'Today is Three Days of the Omer'},
-{hebrew:'הַיּוֹם אַרְבָּעָה יָמִים לָעֹמֶר',english:'Today is Four Days of the Omer'},
-{hebrew:'הַיּוֹם חֲמִשָּׁה יָמִים לָעֹמֶר',english:'Today is Five Days of the Omer'},
-{hebrew:'הַיּוֹם שִׁשָּׁה יָמִים לָעֹמֶר',english:'Today is Six Days of the Omer'},
-{hebrew:'הַיּוֹם שִׁבְעָה יָמִים, שֶׁהֵם שָׁבוּעַ אֶחָד לָעֹמֶר',english:'Today is Seven Days, which are One Week of the Omer'},
-{hebrew:'הַיּוֹם שְׁמוֹנָה יָמִים, שֶׁהֵם שָׁבוּעַ אֶחָד וְיוֹם אֶחָד לָעֹמֶר',english:'Today is Eight Days, which are One Week and One Day of the Omer'},
-{hebrew:'הַיּוֹם תִּשְׁעָה יָמִים, שֶׁהֵם שָׁבוּעַ אֶחָד וּשְׁנֵי יָמִים לָעֹמֶר',english:'Today is Nine Days, which are One Week and Two Days of the Omer'},
-{hebrew:'הַיּוֹם עֲשָׂרָה יָמִים, שֶׁהֵם שָׁבוּעַ אֶחָד וּשְׁלֹשָׁה יָמִים לָעֹמֶר',english:'Today is Ten Days, which are One Week and Three Days of the Omer'},
-{hebrew:'הַיּוֹם אַחַד עָשָׂר יוֹם, שֶׁהֵם שָׁבוּעַ אֶחָד וְאַרְבָּעָה יָמִים לָעֹמֶר',english:'Today is Eleven Days, which are One Week and Four Days of the Omer'},
-{hebrew:'הַיּוֹם שְׁנֵים עָשָׂר יוֹם, שֶׁהֵם שָׁבוּעַ אֶחָד וַחֲמִשָּׁה יָמִים לָעֹמֶר',english:'Today is Twelve Days, which are One Week and Five Days of the Omer'},
-{hebrew:'הַיּוֹם שְׁלֹשָׁה עָשָׂר יוֹם, שֶׁהֵם שָׁבוּעַ אֶחָד וְשִׁשָּׁה יָמִים לָעֹמֶר',english:'Today is Thirteen Days, which are One Week and Six Days of the Omer'},
-{hebrew:'הַיּוֹם אַרְבָּעָה עָשָׂר יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת לָעֹמֶר',english:'Today is Fourteen Days, which are Two Weeks of the Omer'},
-{hebrew:'הַיּוֹם חֲמִשָּׁה עָשָׂר יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת וְיוֹם אֶחָד לָעֹמֶר',english:'Today is Fifteen Days, which are Two Weeks and One Day of the Omer'},
-{hebrew:'הַיּוֹם שִׁשָּׁה עָשָׂר יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת וּשְׁנֵי יָמִים לָעֹמֶר',english:'Today is Sixteen Days, which are Two Weeks and Two Days of the Omer'},
-{hebrew:'הַיּוֹם שִׁבְעָה עָשָׂר יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת וּשְׁלֹשָׁה יָמִים לָעֹמֶר',english:'Today is Seventeen Days, which are Two Weeks and Three Days of the Omer'},
-{hebrew:'הַיּוֹם שְׁמוֹנָה עָשָׂר יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת וְאַרְבָּעָה יָמִים לָעֹמֶר',english:'Today is Eighteen Days, which are Two Weeks and Four Days of the Omer'},
-{hebrew:'הַיּוֹם תִּשְׁעָה עָשָׂר יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת וַחֲמִשָּׁה יָמִים לָעֹמֶר',english:'Today is Nineteen Days, which are Two Weeks and Five Days of the Omer'},
-{hebrew:'הַיּוֹם עֶשְׂרִים יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת וְשִׁשָּׁה יָמִים לָעֹמֶר',english:'Today is Twenty Days, which are Two Weeks and Six Days of the Omer'},
-{hebrew:'הַיּוֹם אֶחָד וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת לָעֹמֶר',english:'Today is Twenty-One Days, which are Three Weeks of the Omer'},
-{hebrew:'הַיּוֹם שְׁנַֽיִם וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת וְיוֹם אֶחָד לָעֹמֶר',english:'Today is Twenty-Two Days, which are Three Weeks and One Day of the Omer'},
-{hebrew:'הַיּוֹם שְׁלֹשָׁה וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת וּשְׁנֵי יָמִים לָעֹמֶר',english:'Today is Twenty-Three Days, which are Three Weeks and Two Days of the Omer'},
-{hebrew:'הַיּוֹם אַרְבָּעָה וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת וּשְׁלֹשָׁה יָמִים לָעֹמֶר',english:'Today is Twenty-Four Days, which are Three Weeks and Three Days of the Omer'},
-{hebrew:'הַיּוֹם חֲמִשָּׁה וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת וְאַרְבָּעָה יָמִים לָעֹמֶר',english:'Today is Twenty-Five Days, which are Three Weeks and Four Days of the Omer'},
-{hebrew:'הַיּוֹם שִׁשָּׁה וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת וַחֲמִשָּׁה יָמִים לָעֹמֶר',english:'Today is Twenty-Six Days, which are Three Weeks and Five Days of the Omer'},
-{hebrew:'הַיּוֹם שִׁבְעָה וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת וְשִׁשָּׁה יָמִים לָעֹמֶר',english:'Today is Twenty-Seven Days, which are Three Weeks and Six Days of the Omer'},
-{hebrew:'הַיּוֹם שְׁמוֹנָה וְעֶשְׂרִים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת לָעֹמֶר',english:'Today is Twenty-Eight Days, which are Four Weeks of the Omer'},
-{hebrew:'הַיּוֹם תִּשְׁעָה וְעֶשְׂרִים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת וְיוֹם אֶחָד לָעֹמֶר',english:'Today is Twenty-Nine Days, which are Four Weeks and One Day of the Omer'},
-{hebrew:'הַיּוֹם שְׁלֹשִׁים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת וּשְׁנֵי יָמִים לָעֹמֶר',english:'Today is Thirty Days, which are Four Weeks and Two Days of the Omer'},
-{hebrew:'הַיּוֹם אֶחָד וּשְׁלֹשִׁים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת וּשְׁלֹשָׁה יָמִים לָעֹמֶר',english:'Today is Thirty-One Days, which are Four Weeks and Three Days of the Omer'},
-{hebrew:'הַיּוֹם שְׁנַיִם וּשְׁלֹשִׁים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת וְאַרְבָּעָה יָמִים לָעֹמֶר',english:'Today is Thirty-Two Days, which are Four Weeks and Four Days of the Omer'},
-{hebrew:'הַיּוֹם שְׁלֹשָׁה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת וַחֲמִשָּׁה יָמִים לָעֹמֶר',english:'Today is Thirty-Three Days, which are Four Weeks and Five Days of the Omer'},
-{hebrew:'הַיּוֹם אַרְבָּעָה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת וְשִׁשָּׁה יָמִים לָעֹמֶר',english:'Today is Thirty-Four Days, which are Four Weeks and Six Days of the Omer'},
-{hebrew:'הַיּוֹם חֲמִשָּׁה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת לָעֹמֶר',english:'Today is Thirty-Five Days, which are Five Weeks of the Omer'},
-{hebrew:'הַיּוֹם שִׁשָּׁה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת וְיוֹם אֶחָד לָעֹמֶר',english:'Today is Thirty-Six Days, which are Five Weeks and One Day of the Omer'},
-{hebrew:'הַיּוֹם שִׁבְעָה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת וּשְׁנֵי יָמִים לָעֹמֶר',english:'Today is Thirty-Seven Days, which are Five Weeks and Two Days of the Omer'},
-{hebrew:'הַיּוֹם שְׁמוֹנָה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת וּשְׁלֹשָׁה יָמִים לָעֹמֶר',english:'Today is Thirty-Eight Days, which are Five Weeks and Three Days of the Omer'},
-{hebrew:'הַיּוֹם תִּשְׁעָה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת וְאַרְבָּעָה יָמִים לָעֹמֶר',english:'Today is Thirty-Nine Days, which are Five Weeks and Four Days of the Omer'},
-{hebrew:'הַיּוֹם אַרְבָּעִים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת וַחֲמִשָּׁה יָמִים לָעֹמֶר',english:'Today is Forty Days, which are Five Weeks and Five Days of the Omer'},
-{hebrew:'הַיּוֹם אֶחָד וְאַרְבָּעִים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת וְשִׁשָּׁה יָמִים לָעֹמֶר',english:'Today is Forty-One Days, which are Five Weeks and Six Days of the Omer'},
-{hebrew:'הַיּוֹם שְׁנַיִם וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת לָעֹמֶר',english:'Today is Forty-Two Days, which are Six Weeks of the Omer'},
-{hebrew:'הַיּוֹם שְׁלֹשָׁה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת וְיוֹם אֶחָד לָעֹמֶר',english:'Today is Forty-Three Days, which are Six Weeks and One Day of the Omer'},
-{hebrew:'הַיּוֹם אַרְבָּעָה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת וּשְׁנֵי יָמִים לָעֹמֶר',english:'Today is Forty-Four Days, which are Six Weeks and Two Days of the Omer'},
-{hebrew:'הַיּוֹם חֲמִשָּׁה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת וּשְׁלֹשָׁה יָמִים לָעֹמֶר',english:'Today is Forty-Five Days, which are Six Weeks and Three Days of the Omer'},
-{hebrew:'הַיּוֹם שִׁשָּׁה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת וְאַרְבָּעָה יָמִים לָעֹמֶר',english:'Today is Forty-Six Days, which are Six Weeks and Four Days of the Omer'},
-{hebrew:'הַיּוֹם שִׁבְעָה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת וַחֲמִשָּׁה יָמִים לָעֹמֶר',english:'Today is Forty-Seven Days, which are Six Weeks and Five Days of the Omer'},
-{hebrew:'הַיּוֹם שְׁמוֹנָה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת וְשִׁשָּׁה יָמִים לָעֹמֶר',english:'Today is Forty-Eight Days, which are Six Weeks and Six Days of the Omer'},
-{hebrew:'הַיּוֹם תִּשְׁעָה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁבְעָה שָׁבוּעוֹת לָעֹמֶר',english:'Today is Forty-Nine Days, which are Seven Weeks of the Omer'},
+var omer = [
+	{ hebrew: 'הַיּוֹם שְׁנֵי יָמִים לָעֹמֶר', english: 'Today is One Day of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁנֵי יָמִים לָעֹמֶר', english: 'Today is Two Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁלֹשָׁה יָמִים לָעֹמֶר', english: 'Today is Three Days of the Omer' },
+	{ hebrew: 'הַיּוֹם אַרְבָּעָה יָמִים לָעֹמֶר', english: 'Today is Four Days of the Omer' },
+	{ hebrew: 'הַיּוֹם חֲמִשָּׁה יָמִים לָעֹמֶר', english: 'Today is Five Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שִׁשָּׁה יָמִים לָעֹמֶר', english: 'Today is Six Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שִׁבְעָה יָמִים, שֶׁהֵם שָׁבוּעַ אֶחָד לָעֹמֶר', english: 'Today is Seven Days, which are One Week of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁמוֹנָה יָמִים, שֶׁהֵם שָׁבוּעַ אֶחָד וְיוֹם אֶחָד לָעֹמֶר', english: 'Today is Eight Days, which are One Week and One Day of the Omer' },
+	{ hebrew: 'הַיּוֹם תִּשְׁעָה יָמִים, שֶׁהֵם שָׁבוּעַ אֶחָד וּשְׁנֵי יָמִים לָעֹמֶר', english: 'Today is Nine Days, which are One Week and Two Days of the Omer' },
+	{ hebrew: 'הַיּוֹם עֲשָׂרָה יָמִים, שֶׁהֵם שָׁבוּעַ אֶחָד וּשְׁלֹשָׁה יָמִים לָעֹמֶר', english: 'Today is Ten Days, which are One Week and Three Days of the Omer' },
+	{ hebrew: 'הַיּוֹם אַחַד עָשָׂר יוֹם, שֶׁהֵם שָׁבוּעַ אֶחָד וְאַרְבָּעָה יָמִים לָעֹמֶר', english: 'Today is Eleven Days, which are One Week and Four Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁנֵים עָשָׂר יוֹם, שֶׁהֵם שָׁבוּעַ אֶחָד וַחֲמִשָּׁה יָמִים לָעֹמֶר', english: 'Today is Twelve Days, which are One Week and Five Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁלֹשָׁה עָשָׂר יוֹם, שֶׁהֵם שָׁבוּעַ אֶחָד וְשִׁשָּׁה יָמִים לָעֹמֶר', english: 'Today is Thirteen Days, which are One Week and Six Days of the Omer' },
+	{ hebrew: 'הַיּוֹם אַרְבָּעָה עָשָׂר יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת לָעֹמֶר', english: 'Today is Fourteen Days, which are Two Weeks of the Omer' },
+	{ hebrew: 'הַיּוֹם חֲמִשָּׁה עָשָׂר יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת וְיוֹם אֶחָד לָעֹמֶר', english: 'Today is Fifteen Days, which are Two Weeks and One Day of the Omer' },
+	{ hebrew: 'הַיּוֹם שִׁשָּׁה עָשָׂר יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת וּשְׁנֵי יָמִים לָעֹמֶר', english: 'Today is Sixteen Days, which are Two Weeks and Two Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שִׁבְעָה עָשָׂר יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת וּשְׁלֹשָׁה יָמִים לָעֹמֶר', english: 'Today is Seventeen Days, which are Two Weeks and Three Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁמוֹנָה עָשָׂר יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת וְאַרְבָּעָה יָמִים לָעֹמֶר', english: 'Today is Eighteen Days, which are Two Weeks and Four Days of the Omer' },
+	{ hebrew: 'הַיּוֹם תִּשְׁעָה עָשָׂר יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת וַחֲמִשָּׁה יָמִים לָעֹמֶר', english: 'Today is Nineteen Days, which are Two Weeks and Five Days of the Omer' },
+	{ hebrew: 'הַיּוֹם עֶשְׂרִים יוֹם, שֶׁהֵם שְׁנֵי שָׁבוּעוֹת וְשִׁשָּׁה יָמִים לָעֹמֶר', english: 'Today is Twenty Days, which are Two Weeks and Six Days of the Omer' },
+	{ hebrew: 'הַיּוֹם אֶחָד וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת לָעֹמֶר', english: 'Today is Twenty-One Days, which are Three Weeks of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁנַֽיִם וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת וְיוֹם אֶחָד לָעֹמֶר', english: 'Today is Twenty-Two Days, which are Three Weeks and One Day of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁלֹשָׁה וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת וּשְׁנֵי יָמִים לָעֹמֶר', english: 'Today is Twenty-Three Days, which are Three Weeks and Two Days of the Omer' },
+	{ hebrew: 'הַיּוֹם אַרְבָּעָה וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת וּשְׁלֹשָׁה יָמִים לָעֹמֶר', english: 'Today is Twenty-Four Days, which are Three Weeks and Three Days of the Omer' },
+	{ hebrew: 'הַיּוֹם חֲמִשָּׁה וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת וְאַרְבָּעָה יָמִים לָעֹמֶר', english: 'Today is Twenty-Five Days, which are Three Weeks and Four Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שִׁשָּׁה וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת וַחֲמִשָּׁה יָמִים לָעֹמֶר', english: 'Today is Twenty-Six Days, which are Three Weeks and Five Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שִׁבְעָה וְעֶשְׂרִים יוֹם, שֶׁהֵם שְׁלֹשָׁה שָׁבוּעוֹת וְשִׁשָּׁה יָמִים לָעֹמֶר', english: 'Today is Twenty-Seven Days, which are Three Weeks and Six Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁמוֹנָה וְעֶשְׂרִים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת לָעֹמֶר', english: 'Today is Twenty-Eight Days, which are Four Weeks of the Omer' },
+	{ hebrew: 'הַיּוֹם תִּשְׁעָה וְעֶשְׂרִים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת וְיוֹם אֶחָד לָעֹמֶר', english: 'Today is Twenty-Nine Days, which are Four Weeks and One Day of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁלֹשִׁים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת וּשְׁנֵי יָמִים לָעֹמֶר', english: 'Today is Thirty Days, which are Four Weeks and Two Days of the Omer' },
+	{ hebrew: 'הַיּוֹם אֶחָד וּשְׁלֹשִׁים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת וּשְׁלֹשָׁה יָמִים לָעֹמֶר', english: 'Today is Thirty-One Days, which are Four Weeks and Three Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁנַיִם וּשְׁלֹשִׁים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת וְאַרְבָּעָה יָמִים לָעֹמֶר', english: 'Today is Thirty-Two Days, which are Four Weeks and Four Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁלֹשָׁה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת וַחֲמִשָּׁה יָמִים לָעֹמֶר', english: 'Today is Thirty-Three Days, which are Four Weeks and Five Days of the Omer' },
+	{ hebrew: 'הַיּוֹם אַרְבָּעָה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם אַרְבָּעָה שָׁבוּעוֹת וְשִׁשָּׁה יָמִים לָעֹמֶר', english: 'Today is Thirty-Four Days, which are Four Weeks and Six Days of the Omer' },
+	{ hebrew: 'הַיּוֹם חֲמִשָּׁה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת לָעֹמֶר', english: 'Today is Thirty-Five Days, which are Five Weeks of the Omer' },
+	{ hebrew: 'הַיּוֹם שִׁשָּׁה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת וְיוֹם אֶחָד לָעֹמֶר', english: 'Today is Thirty-Six Days, which are Five Weeks and One Day of the Omer' },
+	{ hebrew: 'הַיּוֹם שִׁבְעָה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת וּשְׁנֵי יָמִים לָעֹמֶר', english: 'Today is Thirty-Seven Days, which are Five Weeks and Two Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁמוֹנָה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת וּשְׁלֹשָׁה יָמִים לָעֹמֶר', english: 'Today is Thirty-Eight Days, which are Five Weeks and Three Days of the Omer' },
+	{ hebrew: 'הַיּוֹם תִּשְׁעָה וּשְׁלֹשִׁים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת וְאַרְבָּעָה יָמִים לָעֹמֶר', english: 'Today is Thirty-Nine Days, which are Five Weeks and Four Days of the Omer' },
+	{ hebrew: 'הַיּוֹם אַרְבָּעִים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת וַחֲמִשָּׁה יָמִים לָעֹמֶר', english: 'Today is Forty Days, which are Five Weeks and Five Days of the Omer' },
+	{ hebrew: 'הַיּוֹם אֶחָד וְאַרְבָּעִים יוֹם, שֶׁהֵם חֲמִשָּׁה שָׁבוּעוֹת וְשִׁשָּׁה יָמִים לָעֹמֶר', english: 'Today is Forty-One Days, which are Five Weeks and Six Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁנַיִם וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת לָעֹמֶר', english: 'Today is Forty-Two Days, which are Six Weeks of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁלֹשָׁה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת וְיוֹם אֶחָד לָעֹמֶר', english: 'Today is Forty-Three Days, which are Six Weeks and One Day of the Omer' },
+	{ hebrew: 'הַיּוֹם אַרְבָּעָה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת וּשְׁנֵי יָמִים לָעֹמֶר', english: 'Today is Forty-Four Days, which are Six Weeks and Two Days of the Omer' },
+	{ hebrew: 'הַיּוֹם חֲמִשָּׁה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת וּשְׁלֹשָׁה יָמִים לָעֹמֶר', english: 'Today is Forty-Five Days, which are Six Weeks and Three Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שִׁשָּׁה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת וְאַרְבָּעָה יָמִים לָעֹמֶר', english: 'Today is Forty-Six Days, which are Six Weeks and Four Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שִׁבְעָה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת וַחֲמִשָּׁה יָמִים לָעֹמֶר', english: 'Today is Forty-Seven Days, which are Six Weeks and Five Days of the Omer' },
+	{ hebrew: 'הַיּוֹם שְׁמוֹנָה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁשָּׁה שָׁבוּעוֹת וְשִׁשָּׁה יָמִים לָעֹמֶר', english: 'Today is Forty-Eight Days, which are Six Weeks and Six Days of the Omer' },
+	{ hebrew: 'הַיּוֹם תִּשְׁעָה וְאַרְבָּעִים יוֹם, שֶׁהֵם שִׁבְעָה שָׁבוּעוֹת לָעֹמֶר', english: 'Today is Forty-Nine Days, which are Seven Weeks of the Omer' },
 ]
 
-var start = new Date(2022,3,17);
-var d=new Date(Date.now());
-var interval=d.getTime()-start.getTime();
-var days=Math.round(interval/24/60/60/1000);
-var colors = ['#6F1E51','#FFC312','#F79F1F','#EE5A24','#EA2027','#C4E538','#A3CB38','#009432','#006266','#12CBC4','#1289A7','#0652DD','#1B1464','#FDA7DF','#D980FA','#9980FA','#5758BB','#ED4C67','#B53471','#833471','#6F1E51'];
+var start = new Date(2023, 4, 6);
+var d = new Date(Date.now());
+var interval = d.getTime() - start.getTime();
+var days = Math.round(interval / 24 / 60 / 60 / 1000);
+var colors = ['#6F1E51', '#FFC312', '#F79F1F', '#EE5A24', '#EA2027', '#C4E538', '#A3CB38', '#009432', '#006266', '#12CBC4', '#1289A7', '#0652DD', '#1B1464', '#FDA7DF', '#D980FA', '#9980FA', '#5758BB', '#ED4C67', '#B53471', '#833471', '#6F1E51'];
 var body = document.querySelector("body");
 var nav = document.getElementById("colorlabel2")
 document.getElementById('english').textContent = omer[days].english;
 document.getElementById('hebrew').textContent = omer[days].hebrew;
 
 function changeColor() {
-  var i = colors[Math.floor(Math.random() * colors.length)]
-  body.style.backgroundColor = i;
-  nav.style.backgroundColor = i;
-  document.getElementById('colorlabel2').innerHTML = "Sefirat HaOmer " + i;
+	var i = colors[Math.floor(Math.random() * colors.length)]
+	body.style.backgroundColor = i;
+	nav.style.backgroundColor = i;
+	document.getElementById('colorlabel2').innerHTML = "Sefirat HaOmer " + i;
 }
 
 // Register service worker to control making site work offline
 
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
-	  .register('sw.js')
-	  .then(() => { console.log('Service Worker Registered'); });
-  }
-  
-  // Code to handle install prompt on desktop
-  
-  let deferredPrompt;
+		.register('sw.js')
+		.then(() => { console.log('Service Worker Registered'); });
+}
 
-  
-  window.addEventListener('beforeinstallprompt', (e) => {
+// Code to handle install prompt on desktop
+
+let deferredPrompt;
+
+
+window.addEventListener('beforeinstallprompt', (e) => {
 	// Prevent Chrome 67 and earlier from automatically showing the prompt
 	e.preventDefault();
 	// Stash the event so it can be triggered later.
 	deferredPrompt = e;
 	// Update UI to notify the user they can add to home screen
 	addBtn.style.display = 'block';
-  
+
 	addBtn.addEventListener('click', () => {
-	  // hide our user interface that shows our A2HS button
-	  addBtn.style.display = 'none';
-	  // Show the prompt
-	  deferredPrompt.prompt();
-	  // Wait for the user to respond to the prompt
-	  deferredPrompt.userChoice.then((choiceResult) => {
-		if (choiceResult.outcome === 'accepted') {
-		  console.log('User accepted the A2HS prompt');
-		} else {
-		  console.log('User dismissed the A2HS prompt');
-		}
-		deferredPrompt = null;
-	  });
+		// hide our user interface that shows our A2HS button
+		addBtn.style.display = 'none';
+		// Show the prompt
+		deferredPrompt.prompt();
+		// Wait for the user to respond to the prompt
+		deferredPrompt.userChoice.then((choiceResult) => {
+			if (choiceResult.outcome === 'accepted') {
+				console.log('User accepted the A2HS prompt');
+			} else {
+				console.log('User dismissed the A2HS prompt');
+			}
+			deferredPrompt = null;
+		});
 	});
-  });
+});
 
 
-  

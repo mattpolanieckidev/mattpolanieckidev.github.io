350832999c93f1e5179632dfb1b18888f456dbf5
Matt Polaniecki <matpol88@gmail.com>
2023-05-25 10:57:33 -0400

    Revert "got all prayers to load, need to work on styling"
    This reverts commit 24979d78018d1c888ab7a5bb94e2293a680c320b.

--- mysiddurapp/app.js
+++ mysiddurapp/app.js
@@ -1,51 +1,27 @@
-var date, next, hePasuk;
-var pageTitle = document.getElementById("pageTitle");
-var textdiv = document.getElementById("main-content");
-var div, heading, innerDiv, pasuk, newHeading, links;
-var textContent = document.getElementsByClassName("pageText");
-
-function createDiv(){
-    div = document.createElement("div");
-    div.classList.add("textContent");
-    textdiv.appendChild(div);
-    heading = document.createElement("h3");
-    heading.className = "pageHeading";
-    div.appendChild(heading);
-    innerDiv = document.createElement("div");
-    innerDiv.classList.add("pageText");
-    div.appendChild(innerDiv);
-  }
-  
-  //Loop through available pasukim and append to unordered list. Check to see if the translation should be shown or not.
-  function writePasuk() {
-    hePasuk.forEach((pasuk, index) => {
-      const p = document.createElement("p");
-      p.innerHTML = `${hePasuk[index]}<p>${pasuk}</p>`;
-      innerDiv.appendChild(p);
-    });
- 
-  }
-  
 // Recursive function to fetch subsequent parts of the prayer
 function fetchNextPart(url) {
   return fetch(url)
     .then(response => response.json())
-    .then(pull => {
+    .then(data => {
       // Handle the fetched part data here
-      // console.log(data); // Display the data in the console for demonstration
+      console.log(data); // Display the data in the console for demonstration
 
       // You can process the part data and integrate it with your app functionality
       // For example, you can create <p> tags for each returned data and display them in the HTML
-      hePasuk = pull.he;
-      createDiv();
-      heading.innerHTML = pull.ref;
-      writePasuk();
 
+      const mainContentElement = document.getElementById('main-content');
+
+      // Create a <p> element for the current part of the prayer
+      const pTag = document.createElement('p');
+      pTag.textContent = data.he;
+
+      // Append the <p> element to the main content element
+      mainContentElement.appendChild(pTag);
 
       // Check if there is a next part
-      if (pull.next) {
+      if (data.next) {
         // Fetch the next part recursively
-        return fetchNextPart(`https://www.sefaria.org/api/texts/${pull.next}`);
+        return fetchNextPart(`https://www.sefaria.org/api/texts/${data.next}`);
       }
     })
     .catch(error => {
@@ -56,4 +32,3 @@ function fetchNextPart(url) {
 
 // Fetch Ashkenazi Siddur - Modeh Ani from Sefaria API
 fetchNextPart('https://www.sefaria.org/api/texts/Siddur_Ashkenaz%2C_Weekday%2C_Shacharit%2C_Preparatory_Prayers%2C_Modeh_Ani?ven=Artscroll_siddur&lang=bi');
-

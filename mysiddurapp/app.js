740aa6c8fe87522ce53d1399a9b0e62c08c82011
Matt Polaniecki <matpol88@gmail.com>
2023-05-25 12:01:57 -0400

    filtered for shacharis!

--- mysiddurapp/app.js
+++ mysiddurapp/app.js
@@ -1,34 +1,45 @@
+var pageTitle = document.getElementById("pageTitle");
+var textdiv = document.getElementById("main-content");
+var textContent = document.getElementsByClassName("pageText");
+
 // Recursive function to fetch subsequent parts of the prayer
 function fetchNextPart(url) {
-  return fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      // Handle the fetched part data here
-      console.log(data); // Display the data in the console for demonstration
-
-      // You can process the part data and integrate it with your app functionality
-      // For example, you can create <p> tags for each returned data and display them in the HTML
-
-      const mainContentElement = document.getElementById('main-content');
-
-      // Create a <p> element for the current part of the prayer
-      const pTag = document.createElement('p');
-      pTag.textContent = data.he;
-
-      // Append the <p> element to the main content element
-      mainContentElement.appendChild(pTag);
-
-      // Check if there is a next part
-      if (data.next) {
-        // Fetch the next part recursively
-        return fetchNextPart(`https://www.sefaria.org/api/texts/${data.next}`);
-      }
-    })
-    .catch(error => {
-      // Handle any errors that occur during the fetch
-      console.error('Error fetching part of the prayer:', error);
-    });
-}
-
+    return fetch(url)
+      .then(response => response.json())
+      .then(pull => {
+  const titleVariants = pull.ref;
+  const isWeekdayShacharit = titleVariants.includes("Weekday, Shacharit");
+  
+  if (isWeekdayShacharit) {
+    const div = document.createElement("div");
+    div.classList.add("textContent");
+    textdiv.appendChild(div);
+    const heading = document.createElement("h3");
+    heading.className = "pageHeading";
+    div.appendChild(heading);
+    const innerDiv = document.createElement("div");
+    innerDiv.classList.add("pageText");
+    div.appendChild(innerDiv);
+    const hePasuk = pull.he;
+    heading.innerHTML = pull.heTitleVariants;
+    hePasuk.forEach(pasuk => {
+        const p = document.createElement("p");
+        p.innerHTML = pasuk;
+        innerDiv.appendChild(p);
+      });
+  }
+  
+        // Check if there is a next part
+        if (pull.next) {
+          // Fetch the next part recursively
+          return fetchNextPart(`https://www.sefaria.org/api/texts/${pull.next}`);
+        }
+      })
+      .catch(error => {
+        // Handle any errors that occur during the fetch
+        console.error('Error fetching part of the prayer:', error);
+      });
+  }
+  
 // Fetch Ashkenazi Siddur - Modeh Ani from Sefaria API
 fetchNextPart('https://www.sefaria.org/api/texts/Siddur_Ashkenaz%2C_Weekday%2C_Shacharit%2C_Preparatory_Prayers%2C_Modeh_Ani?ven=Artscroll_siddur&lang=bi');

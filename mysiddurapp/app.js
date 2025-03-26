925c37f86cbd4cccf13be97b213b28d60f7b3033
Polaniecki, Matthew <matpol88@gmail.com>
2025-03-26 14:43:57 -0400

    no fetch

--- mysiddurapp/app.js
+++ mysiddurapp/app.js
@@ -1,14 +1,3 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker.register("service-worker.js").then(function(registration) {
-      // Registration was successful
-      console.log("ServiceWorker registration successful with scope: ", registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log("ServiceWorker registration failed: ", err);
-    });
-  });
-}
 
 var slider = document.getElementById("fontSize");
 var fontSize = slider.value;
@@ -16,48 +5,7 @@ var pageTitle = document.getElementById("pageTitle");
 var textdiv = document.getElementById("main-content");
 var textContent = document.getElementsByClassName("pageText");
 
-// Recursive function to fetch subsequent parts of the prayer
-function fetchNextPart(url) {
-    return fetch(url)
-      .then(response => response.json())
-      .then(pull => {
-        const refVariants = pull.ref;
-        const isWeekdayShacharit = refVariants.includes("Weekday, Shacharit");
-  
-        if (isWeekdayShacharit) {
-          const div = document.createElement("div");
-          div.classList.add("textContent");
-          textdiv.appendChild(div);
-          const heading = document.createElement("h3");
-          heading.className = "pageHeading";
-          heading.setAttribute("data-anchor", pull.ref.replace(/\s+/g, "-").toLowerCase()); // Set data-anchor attribute
-          div.appendChild(heading);
-          const innerDiv = document.createElement("div");
-          innerDiv.classList.add("pageText");
-          div.appendChild(innerDiv);
-          const hePasuk = pull.he;
-          heading.innerHTML = pull.heTitleVariants; // Use pull.ref instead of pull.heTitleVariants
-          hePasuk.forEach(pasuk => {
-            const p = document.createElement("p");
-            p.innerHTML = pasuk;
-            innerDiv.appendChild(p);
-            adjustFont(localStorage.getItem("size"));
-          });
-  
-       //   addSectionToList(pull.ref); // Add section ref to the navigation list
-        }
-  
-        // Check if there is a next part
-        if (pull.next) {
-          // Fetch the next part recursively
-          return fetchNextPart(`https://www.sefaria.org/api/texts/${pull.next}`);
-        }
-      })
-      .catch(error => {
-        // Handle any errors that occur during the fetch
-        console.error('Error fetching part of the prayer:', error);
-      });
-  }
+
   
   // Check if the navigation menu is open
   const navbarCollapse = document.querySelector(".navbar-collapse.show");
@@ -70,9 +18,6 @@ function fetchNextPart(url) {
       }
     });
   }
-
-// Fetch Ashkenazi Siddur - Modeh Ani from Sefaria API
-fetchNextPart('https://www.sefaria.org/api/texts/Siddur_Ashkenaz%2C_Weekday%2C_Shacharit%2C_Preparatory_Prayers%2C_Modeh_Ani?ven=Artscroll_siddur&lang=bi');
 
 function adjustFontSize(size) {
   const textContentElements = document.getElementsByClassName("pageText");

7007135dffca29ec6918f6ec69492f6170fb2ac5
Polaniecki, Matthew <matpol88@gmail.com>
2025-03-26 15:41:59 -0400

    fixing mincha

--- mysiddurapp/mincha.js
+++ mysiddurapp/mincha.js
@@ -1,48 +1,10 @@
 var pageTitle = document.getElementById("pageTitle");
 var textdiv = document.getElementById("main-content");
 var textContent = document.getElementsByClassName("pageText");
+var slider = document.getElementById("fontSize");
+var fontSize = slider.value;
 
-// Recursive function to fetch subsequent parts of the prayer
-function fetchNextPart(url) {
-    return fetch(url)
-      .then(response => response.json())
-      .then(pull => {
-        const refVariants = pull.ref;
-        const isWeekdayShacharit = refVariants.includes("Weekday, Mincha");
-  
-        if (isWeekdayShacharit) {
-          const div = document.createElement("div");
-          div.classList.add("textContent");
-          textdiv.appendChild(div);
-          const heading = document.createElement("h3");
-          heading.className = "pageHeading";
-          heading.setAttribute("data-anchor", pull.ref.replace(/\s+/g, "-").toLowerCase()); // Set data-anchor attribute
-          div.appendChild(heading);
-          const innerDiv = document.createElement("div");
-          innerDiv.classList.add("pageText");
-          div.appendChild(innerDiv);
-          const hePasuk = pull.he;
-          heading.innerHTML = pull.heTitleVariants; // Use pull.ref instead of pull.heTitleVariants
-          hePasuk.forEach(pasuk => {
-            const p = document.createElement("p");
-            p.innerHTML = pasuk;
-            innerDiv.appendChild(p);
-          });
-  
-         // addSectionToList(pull.ref); // Add section ref to the navigation list
-        }
-  
-        // Check if there is a next part
-        if (pull.next) {
-          // Fetch the next part recursively
-          return fetchNextPart(`https://www.sefaria.org/api/texts/${pull.next}`);
-        }
-      })
-      .catch(error => {
-        // Handle any errors that occur during the fetch
-        console.error('Error fetching part of the prayer:', error);
-      });
-  }
+
   
   
 
@@ -75,11 +37,6 @@ function fetchNextPart(url) {
   }
 
 
-
-
-// Fetch Ashkenazi Siddur - Modeh Ani from Sefaria API
-fetchNextPart('https://www.sefaria.org/api/texts/Siddur_Ashkenaz%2C_Weekday%2C_Minchah%2C_Ashrei?lang=he');
-
 function adjustFontSize(size) {
   const textContentElements = document.getElementsByClassName("pageText");
   for (let i = 0; i < textContentElements.length; i++) {
@@ -87,38 +44,52 @@ function adjustFontSize(size) {
   }
 }
 
-function adjustFontSize(size) {
-  const textContentElements = document.getElementsByClassName("pageText");
-  for (let i = 0; i < textContentElements.length; i++) {
-    textContentElements[i].style.fontSize = size + "px";
+//set slider value on mobile
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    adjustFont("1");
+  } else {
+    adjustFont("2");
   }
 }
 
-// Event listener for font size change
-document.addEventListener("DOMContentLoaded", () => {
-  const fontSizeButtons = document.querySelectorAll(".font-size-btn");
+var x = window.matchMedia("(orientation:portrait)")
+myFunction(x)
+x.addListener(myFunction)
 
-  fontSizeButtons.forEach(button => {
-    button.addEventListener("click", event => {
-      const fontSize = event.target.dataset.fontSize;
-      adjustFontSize(fontSize);
-      // Save the font size to local storage
-      localStorage.setItem("fontSize", fontSize);
+//adjust font size based on slider value
+function adjustFont(a){
+  for (i = 0; i < textContent.length; i++) {
+    if (a === "1"){
+      slider.value="1";
+      localStorage.setItem("size", "1");
+      textContent.item(i).style.fontSize="24px";
+    }
+    else if (a === "2"){
+      slider.value="2";
+      localStorage.setItem("size", "2");
+      textContent.item(i).style.fontSize="30px";
+    }
+    else if (a === "3"){
+      localStorage.setItem("size", "3");
+      textContent.item(i).style.fontSize="36px";
+    }
+    }
+  };
+
+  // Accordion
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
     });
-  });
-});
-
-// Event listener before page unload to save font size
-window.addEventListener("beforeunload", () => {
-  const currentFontSize = getComputedStyle(document.querySelector(".pageText")).fontSize;
-  localStorage.setItem("fontSize", parseInt(currentFontSize));
-});
-
-// Apply font size on page load
-document.addEventListener("DOMContentLoaded", () => {
-  const storedFontSize = localStorage.getItem("fontSize");
-  if (storedFontSize) {
-    adjustFontSize(storedFontSize);
   }
-});
 

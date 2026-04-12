ce21b127156d7cba8a949469c60d33b826305fb6
Matt Polaniecki <matpol88@gmail.com>
2026-04-12 19:07:28 -0400

    Add files via upload

---
+++ r100/sw.js
@@ -0,0 +1,9 @@
+const C='road100-v1';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)).catch(()=>{}));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const copy=res.clone();caches.open(C).then(c=>c.put(e.request,copy)).catch(()=>{});return res;
+  }).catch(()=>caches.match('./index.html'))));
+});
